1fcef1d5e1a2c348bfec2eb8c2bda9e745be9c37
浅爱 <shallowlove21@gmail.com>
2018-03-12 16:48:54 +0800

    commit

--- layerDemo5/ViewController.m
+++ layerDemo5/ViewController.m
@@ -9,6 +9,10 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *minHand;
+@property (weak, nonatomic) IBOutlet UIImageView *hourHand;
+@property (weak, nonatomic) IBOutlet UIImageView *secHand;
+@property (weak, nonatomic) NSTimer              *timer;
 
 @end
 
@@ -16,13 +20,37 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    //调整anchorPoint
+    self.hourHand.layer.anchorPoint = CGPointMake(0.5f, 0.9f);
+    self.minHand.layer.anchorPoint = CGPointMake(0.5f, 0.9f);
+    self.secHand.layer.anchorPoint = CGPointMake(0.5f, 0.9f);
+    
+    //开启定时器
+    self.timer = [NSTimer scheduledTimerWithTimeInterval:1.0 target:self selector:@selector(tick) userInfo:nil repeats:YES];
+    
+    //初始化位置
+    [self tick];
+
 }
 
+//钟表
+- (void)tick {
+    //时间换算
+    NSCalendar *calendar = [[NSCalendar alloc] initWithCalendarIdentifier:NSCalendarIdentifierGregorian];
+    NSUInteger units = NSCalendarUnitHour | NSCalendarUnitMinute | NSCalendarUnitSecond;
+    NSDateComponents *components = [calendar components:units fromDate:[NSDate date]];
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    CGFloat hourAngle = (components.hour / 12.0) * M_PI * 2.0;
+    CGFloat minuAngle = (components.minute / 60.0) * M_PI * 2.0;
+    CGFloat secdAngle = (components.second / 60.0) *M_PI * 2.0;
+    
+    //旋转指针
+    self.hourHand.transform = CGAffineTransformMakeRotation(hourAngle);
+    self.minHand.transform = CGAffineTransformMakeRotation(minuAngle);
+    self.secHand.transform = CGAffineTransformMakeRotation(secdAngle);
+    
+    
 }
 
 
